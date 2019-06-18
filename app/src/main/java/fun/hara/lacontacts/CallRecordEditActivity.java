c6e556bba3ab39eb78f4d298fe5cb67208a40611
hanaiii <hanaii@126.com>
2019-06-18 22:51:15 +0800

    添加了通话记录相关的UI

---
+++ app/src/main/java/fun/hara/lacontacts/CallRecordEditActivity.java
@@ -0,0 +1,13 @@
+package fun.hara.lacontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallRecordEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_record_edit);
+    }
+}
