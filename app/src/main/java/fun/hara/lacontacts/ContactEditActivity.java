55967d326ce0e3f8e4b14c5771897ed4032e335e
zc z <670222454@qq.com>
2019-06-18 20:53:51 +0800

    拨号功能功能

---
+++ app/src/main/java/fun/hara/lacontacts/ContactEditActivity.java
@@ -0,0 +1,291 @@
+package fun.hara.lacontacts;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.media.Image;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.common.Constant;
+
+import fun.hara.lacontacts.dao.ContactsDAO;
+import fun.hara.lacontacts.domain.ContactInfo;
+import fun.hara.lacontacts.util.QRCodeUtil;
+
+public class ContactEditActivity extends AppCompatActivity {
+    private static final String DECODED_CONTENT_KEY = "codedContent";
+    private static final String DECODED_BITMAP_KEY = "codedBitmap";
+    private static final int REQUEST_CODE_SCAN = 0x0000;
+    private ContactsDAO contactsDAO;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_contact_edit);
+        init();
+    }
+
+    /**
+     * 初始化操作
+     */
+    private void init(){
+        contactsDAO = new ContactsDAO(this);
+        Intent intent = getIntent();
+        // 回显信息
+        String name = intent.getStringExtra("name");
+        String phone = intent.getStringExtra("phone");
+        EditText nameET = (EditText) findViewById(R.id.editContactName);
+        EditText phoneET = (EditText) findViewById(R.id.editContactPhone);
+        nameET.setText(name);
+        phoneET.setText(phone);
+        String id = intent.getStringExtra("id");
+        if(id!=null && !"".equals(id)){
+            // id存在时
+            TextView idTV = (TextView)findViewById(R.id.contactId);
+            idTV.setText(id);
+            // 修改标题
+            TextView editContactTitle = (TextView) findViewById(R.id.editContactTitle);
+            editContactTitle.setText("编辑联系人");
+            // 禁用编辑
+            uneditable(nameET, phoneET);
+            // 生成二维码
+            ImageView QRCodeImg = (ImageView) findViewById(R.id.QRCodeImg);
+            QRCodeImg.setVisibility(View.VISIBLE);
+            Bitmap QRCodeBitmap = QRCodeUtil.createQRCodeBitmap(JSON.toJSONString(new ContactInfo(0,name,phone)), 480, 480);
+            QRCodeImg.setImageBitmap(QRCodeBitmap);
+            // 显示底部操作区
+            RelativeLayout bottom = (RelativeLayout) findViewById(R.id.editContactBottom);
+            bottom.setVisibility(View.VISIBLE);
+        }else{
+            /* 新增联系人 */
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions((Activity) this,
+                        new String[]{android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            }
+            ImageButton editModeBtn = (ImageButton) findViewById(R.id.editModeBtn);
+            ImageButton saveContactBtn = (ImageButton) findViewById(R.id.saveContactBtn);
+            editModeBtn.setVisibility(View.GONE);
+            saveContactBtn.setVisibility(View.VISIBLE);
+            ImageButton QRCodeScanBtn = (ImageButton) findViewById(R.id.QRCodeScanBtn);
+            QRCodeScanBtn.setVisibility(View.VISIBLE);
+        }
+        //拨号功能，不懂你怎么不调用按键点击就能触发的
+        ImageButton callBtn = (ImageButton)findViewById(R.id.callBtn);
+        final EditText telePhone = (EditText) findViewById(R.id.editContactPhone);
+        callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                //设置拨打电话的动作
+                intent.setAction(Intent.ACTION_CALL);
+                //设置拨打电话的号码
+                intent.setData(Uri.parse("tel:" + telePhone.getText()));
+                //开启打电话的意图
+                startActivity(intent);
+            }
+        });
+    }
+
+    /**
+     * 普通回退
+     * @param view
+     */
+    public void back(View view){
+        setResult(0);
+        finish();
+    }
+
+    /**
+     * 保存联系人
+     * @param view
+     */
+    public void saveContact(View view){
+        EditText nameET = (EditText) findViewById(R.id.editContactName);
+        EditText phoneET = (EditText) findViewById(R.id.editContactPhone);
+        String name = nameET.getText().toString();
+        String phone = phoneET.getText().toString();
+        // 空值检查
+        if(TextUtils.isEmpty(name) || TextUtils.isEmpty(phone)){
+            Toast.makeText(this,"姓名或电话不能为空！",Toast.LENGTH_LONG ).show();
+            return;
+        }
+        contactsDAO.save(new ContactInfo(0, name, phone));
+        setResult(1);
+        finish();
+    }
+
+    /**
+     * 删除联系人
+     * @param view
+     */
+    public void deleteContact(View view){
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("提示")
+                .setMessage("确定删除该联系人吗？")
+                .setPositiveButton("确定",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        TextView idTV = (TextView)findViewById(R.id.contactId);
+                        Integer id = Integer.parseInt(idTV.getText().toString());
+                        contactsDAO.delete(id);
+                        setResult(0);
+                        finish();
+                    }
+                })
+                .setNegativeButton("取消",null)
+                .create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.BLACK);
+        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(Color.GRAY);
+    }
+
+    /**
+     * 切换到编辑模式
+     * @param view
+     */
+    public void switchEditMode(View view){
+        EditText nameET = (EditText) findViewById(R.id.editContactName);
+        EditText phoneET = (EditText) findViewById(R.id.editContactPhone);
+        view.setVisibility(View.GONE);
+        ImageButton updateContactBtn = (ImageButton) findViewById(R.id.updateContactBtn);
+        ImageButton cancelEditBtn1 = (ImageButton) findViewById(R.id.cancelEditBtn1);
+        updateContactBtn.setVisibility(View.VISIBLE);
+        cancelEditBtn1.setVisibility(View.VISIBLE);
+        editable(nameET, phoneET);
+        RelativeLayout editContactBottom = (RelativeLayout) findViewById(R.id.editContactBottom);
+        editContactBottom.setVisibility(View.GONE);
+    }
+
+    /**
+     * 退出更新模式
+     * @param view
+     */
+    public void cancelEditMode(View view){
+        EditText nameET = (EditText) findViewById(R.id.editContactName);
+        EditText phoneET = (EditText) findViewById(R.id.editContactPhone);
+        view.setVisibility(View.GONE);
+        ImageButton updateContactBtn = (ImageButton) findViewById(R.id.updateContactBtn);
+        ImageButton editModeBtn = (ImageButton) findViewById(R.id.editModeBtn);
+        updateContactBtn.setVisibility(View.GONE);
+        editModeBtn.setVisibility(View.VISIBLE);
+        RelativeLayout editContactBottom = (RelativeLayout) findViewById(R.id.editContactBottom);
+        editContactBottom.setVisibility(View.VISIBLE);
+        uneditable(nameET, phoneET);
+
+        nameET.setText(getIntent().getStringExtra("name"));
+        phoneET.setText( getIntent().getStringExtra("phone"));
+    }
+
+    /**
+     * 更新联系人
+     * @param view
+     */
+    public void updateContact(View view){
+        EditText nameET = (EditText) findViewById(R.id.editContactName);
+        EditText phoneET = (EditText) findViewById(R.id.editContactPhone);
+        String name = nameET.getText().toString();
+        String phone = phoneET.getText().toString();
+        TextView idTV = (TextView)findViewById(R.id.contactId);
+        Integer id = Integer.parseInt(idTV.getText().toString());
+        // 空值检查
+        if(TextUtils.isEmpty(name) || TextUtils.isEmpty(phone)){
+            Toast.makeText(this,"姓名或电话不能为空！",Toast.LENGTH_LONG ).show();
+            return;
+        }
+        contactsDAO.update(new ContactInfo(id, name, phone));
+        setResult(2);
+        finish();
+    }
+
+    /**
+     * 扫描二维码
+     * @param view
+     */
+    public void scan(View view){
+        Intent intent = new Intent(this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_SCAN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                String name = null;
+                String phone = null;
+                try{
+                    ContactInfo contactInfo = JSON.parseObject(content, ContactInfo.class);
+                     name = contactInfo.getName();
+                     phone = contactInfo.getPhone();
+                }catch (RuntimeException e){
+                    Toast.makeText(this,"二维码格式错误！",Toast.LENGTH_LONG ).show();
+                    return;
+                }
+                // 空值检查
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(phone)){
+                    Toast.makeText(this,"二维码格式错误！",Toast.LENGTH_LONG ).show();
+                    return;
+                }
+                TextView nameTV = (TextView)findViewById(R.id.editContactName);
+                TextView phoneTV = (TextView)findViewById(R.id.editContactPhone);
+                nameTV.setText(name);
+                phoneTV.setText(phone);
+            }
+        }
+    }
+
+
+    /**
+     * 让EditText不可编辑
+     * @param editTexts
+     */
+    private void uneditable(EditText ... editTexts){
+        for (EditText editText : editTexts) {
+            editText.setFocusable(false);
+            editText.setFocusableInTouchMode(false);
+        }
+
+    }
+
+    /**
+     * 让EditText可被编辑
+     * @param editTexts
+     */
+    private void editable(EditText ... editTexts){
+        for (EditText editText : editTexts) {
+            editText.setFocusableInTouchMode(true);
+            editText.setFocusable(true);
+            editText.requestFocus();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/fun/hara/lacontacts/MainActivity.java
@@ -0,0 +1,193 @@
+package fun.hara.lacontacts;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.common.Constant;
+
+
+
+import fun.hara.lacontacts.domain.ContactInfo;
+import fun.hara.lacontacts.fragment.CallFragment;
+import fun.hara.lacontacts.fragment.ContactsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private CallFragment callFragment;
+    private ContactsFragment contactsFragment;
+
+
+    /**
+     * 初始化相关fragment
+     */
+    public void initFragment() {
+        callFragment = new CallFragment();
+        contactsFragment = new ContactsFragment();
+        // 开启一个事务将fragment动态加载到组件
+        FragmentTransaction tx = getSupportFragmentManager().beginTransaction();
+        tx.add(R.id.content, callFragment).add(R.id.content, contactsFragment);
+        // 隐藏fragment
+        tx.hide(callFragment).hide(contactsFragment);
+        //返回到上一个显示的fragment
+        tx.addToBackStack(null);
+        tx.commit();
+        // 展示通话界面
+        switchNav(R.id.navigation_call);
+    }
+
+
+    /**
+     * 切换导航对应的fragment
+     *
+     * @param navId
+     */
+    public void switchNav(int navId) {
+        FragmentTransaction beginTransaction = getSupportFragmentManager().beginTransaction();
+        switch (navId) {
+            case R.id.navigation_call:
+                beginTransaction.hide(contactsFragment);
+                beginTransaction.show(callFragment);
+                break;
+            case R.id.navigation_contacts:
+                beginTransaction.hide(callFragment);
+                beginTransaction.show(contactsFragment);
+                break;
+        }
+        beginTransaction.addToBackStack(null);
+        beginTransaction.commit();
+
+    }
+
+
+    /**
+     * 应用权限检查
+     */
+    private void checkPermission() {
+        // 权限
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CONTACTS)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions((Activity) this,
+                    new String[]{android.Manifest.permission.READ_CONTACTS, android.Manifest.permission.WRITE_CONTACTS,android.Manifest.permission.CAMERA, android.Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CALL_PHONE},
+                    1);
+        }
+
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_call:
+                    switchNav(R.id.navigation_call);
+                    return true;
+                case R.id.navigation_contacts:
+                    switchNav(R.id.navigation_contacts);
+                    return true;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermission();  // 权限检查
+        setContentView(R.layout.activity_main);
+        initFragment();     // 初始化fragment
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+
+
+    public void showPopupMenu(View view) {
+        // View当前PopupMenu显示的相对View的位置
+        PopupMenu popupMenu = new PopupMenu(this, view);
+        // menu布局
+        popupMenu.getMenuInflater().inflate(R.menu.main, popupMenu.getMenu());
+        // menu的item点击事件
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                switch (itemId){
+                    // 设置每个item的响应事件
+                    case R.id.action_scan:
+                        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+                        startActivityForResult(intent, 0);
+                        break;
+                }
+                return false;
+            }
+        });
+        // PopupMenu关闭事件
+        popupMenu.setOnDismissListener(new PopupMenu.OnDismissListener() {
+            @Override
+            public void onDismiss(PopupMenu menu) {
+            }
+        });
+        popupMenu.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            if (data != null) {
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                String name = null;
+                String phone = null;
+                ContactInfo contactInfo = null;
+                try{
+                     contactInfo = JSON.parseObject(content, ContactInfo.class);
+                    name = contactInfo.getName();
+                    phone = contactInfo.getPhone();
+                }catch (RuntimeException e){
+                    Toast.makeText(this,"扫描结果: " + content,Toast.LENGTH_LONG ).show();
+                    return;
+                }
+                // 空值检查
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(phone)){
+                    Toast.makeText(this,"扫描结果: " + content,Toast.LENGTH_LONG ).show();
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, ContactEditActivity.class);
+                intent.putExtra("name", contactInfo.getName());
+                intent.putExtra("phone", contactInfo.getPhone());
+                startActivity(intent);
+            }
+        }
+    }
+}
